46019ef48b7020d1d170f3e59ab771ea7f216eef
Georgios Tsirigos <geo.tsirigos@gmail.com>
2023-01-09 12:38:53 +0200

    Update excel_merger_web_app.py

--- excel_merger_web_app.py
+++ excel_merger_web_app.py
@@ -24,4 +24,39 @@ def download_file(data, file_format):
         b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
         href = f'<a href="data:file/csv;base64,{b64}" download="merged_file.csv">Download CSV File</a>'
         st.markdown(href, unsafe_allow_html=True)
-    elif file_format == "XLS
+    elif file_format == "XLSX":
+        xlsx = openpyxl.Workbook()
+        sheet = xlsx.active
+        for r in dataframe_to_rows(data, index=False, header=True):
+            sheet.append(r)
+        xlsx.save("merged_file.xlsx")
+        st.markdown(
+            f'<a href="/downloads/merged_file.xlsx" download>Download XLSX File</a>',
+            unsafe_allow_html=True,
+        )
+
+st.title("File Merger")
+
+file1 = st.file_uploader("Upload first file")
+file2 = st.file_uploader("Upload second file")
+
+df1 = pd.read_csv(file1)
+df2 = pd.read_csv(file2)
+merged = pd.merge(df1, df2)
+
+column = st.selectbox("Select column to merge on", merged.columns)
+
+select_columns = st.checkbox("Select specific columns to include in merged file")
+
+if select_columns:
+    selected_columns = select_columns(merged)
+else:
+    selected_columns = None
+
+if st.button("Merge files"):
+    merged = merge_files(file1, file2, column, selected_columns)
+    st.dataframe(merged)
+
+if st.button("Download merged file"):
+    format = st.radio("Select format", ["CSV", "XLSX"])
+    download_file(merged, format)
