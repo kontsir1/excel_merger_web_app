3c4154c8e283cd1fad738d6052b56d1f6fbcf38b
Georgios Tsirigos <geo.tsirigos@gmail.com>
2023-01-09 20:33:17 +0200

    Update excel_merger_web_app.py

--- excel_merger_web_app.py
+++ excel_merger_web_app.py
@@ -4,7 +4,11 @@ import base64
 
 def merge_dataframes(df1, df2, common_column1, common_column2):
     # Merge the two dataframes on the specified common columns
-    df_merged = pd.merge(df1, df2, left_on=common_column1, right_on=common_column2, how='inner')
+    try:
+        df_merged = pd.merge(df1, df2, left_on=common_column1, right_on=common_column2, how='inner')
+    except ValueError:
+        st.error("Error: Could not merge dataframes. Make sure the selected common columns exist and have the same data type in both dataframes.")
+        return
     
     # Drop one of the common columns
     df_merged.drop(common_column1, axis=1, inplace=True)
@@ -22,7 +26,11 @@ if file_one and file_two:
     # Read both files as pandas dataframes
     df1 = pd.read_csv(file_one)
     df2 = pd.read_csv(file_two)
-
+    # Check if either dataframe is empty
+    if df1.empty or df2.empty:
+        st.error("Error: One or both of the selected files are empty.")
+        return
+    
     # Display the two dataframes side by side
     col1, col2 = st.columns(2)
     with col1:
@@ -36,18 +44,28 @@ if file_one and file_two:
     common_column1 = st.sidebar.selectbox("Select common column for first file", df1.columns)
     common_column2 = st.sidebar.selectbox("Select common column for second file", df2.columns)
     
+    # Check if either common column is not present in both dataframes
+    if common_column1 not in df1.columns or common_column2 not in df2.columns:
+        st.error("Error: One or both of the selected common columns are not present in both dataframes.")
+        return
+    
     # Merge the dataframes on the selected common columns
     df_merged = merge_dataframes(df1, df2, common_column1, common_column2)
+    
+    # Check if the merged dataframe is empty
+    if df_merged.empty:
+        st.error("Error: No common rows found between the two dataframes.")
+        return
     
     # Display the merged dataframe
     st.markdown("## Merged dataframe")
     st.write(df_merged)
     
-     # Add a download button to the sidebar
+    # Add a download button to the sidebar
     st.sidebar.markdown("## Download merged dataframe")
     if st.sidebar.button("Download CSV"):
         st.markdown("Clicked download button")
         csv = df_merged.to_csv(index=False)
-        b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
+        b64 = base64.b64encode(csv.encode()).decode()
         href = f'<a href="data:file/csv;base64,{b64}" download="merged_data.csv">Download CSV File</a>'
         st.markdown(href, unsafe_allow_html=True)
