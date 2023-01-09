d74ce77c1e984c94819a29e1d0091c953364f958
Georgios Tsirigos <geo.tsirigos@gmail.com>
2023-01-09 15:12:15 +0200

    Update excel_merger_web_app.py

--- excel_merger_web_app.py
+++ excel_merger_web_app.py
@@ -5,45 +5,36 @@ import base64
 
 from openpyxl.utils.dataframe import dataframe_to_rows
 
-import io
-import base64
-
-def merge_files(file1, file2):
-    # Check if file1 or file2 is None
-    if file1 is None or file2 is None:
-        st.error("Please upload both files before merging.")
-        return
-
+def merge_files(file1, file2, column, selected_columns):
     # Check if file1 is a CSV file
-    if file1.name.endswith(".csv"):
-        # Get file1 as a file-like object
-        file1_like = io.BytesIO(file1.get_bytestream())
-        df1 = pd.read_csv(file1_like)
+    if file1.filename.endswith(".csv"):
+        df1 = pd.read_csv(file1)
     # Check if file1 is an Excel file
-    elif file1.name.endswith(".xlsx"):
-        # Get file1 as a file-like object
-        file1_like = io.BytesIO(file1.get_bytestream())
-        df1 = pd.read_excel(file1_like)
+    elif file1.filename.endswith(".xlsx"):
+        df1 = pd.read_excel(file1)
     else:
         st.error("Unsupported file type for file1. Please upload a CSV or Excel file.")
         return
 
     # Check if file2 is a CSV file
-    if file2.name.endswith(".csv"):
-        # Get file2 as a file-like object
-        file2_like = io.BytesIO(file2.get_bytestream())
-        df2 = pd.read_csv(file2_like)
+    if file2.filename.endswith(".csv"):
+        df2 = pd.read_csv(file2)
     # Check if file2 is an Excel file
-    elif file2.name.endswith(".xlsx"):
-        # Get file2 as a file-like object
-        file2_like = io.BytesIO(file2.get_bytestream())
-        df2 = pd.read_excel(file2_like)
+    elif file2.filename.endswith(".xlsx"):
+        df2 = pd.read_excel(file2)
     else:
         st.error("Unsupported file type for file2. Please upload a CSV or Excel file.")
         return
 
-    merged = pd.merge(df1, df2, how='inner')
+    merged = pd.merge(df1, df2, on=column)
+    if selected_columns:
+        merged = merged[selected_columns]
     return merged
+
+def select_columns(dataframe):
+    all_columns = list(dataframe.columns)
+    selected_columns = st.multiselect("Select columns to include in merged file", all_columns)
+    return selected_columns
 
 def download_file(data, file_format):
     if file_format == "CSV":
@@ -66,11 +57,17 @@ st.title("File Merger")
 
 file1 = st.sidebar.file_uploader("Upload first file")
 file2 = st.sidebar.file_uploader("Upload second file")
+column = st.sidebar.selectbox("Select column to merge on", [])
 
+select_columns = st.sidebar.checkbox("Select specific columns to include in merged file")
 
+if select_columns:
+    selected_columns = select_columns(merged)
+else:
+    selected_columns = None
 
 if st.sidebar.button("Merge files"):
-    merged = merge_files(file1, file2)
+    merged = merge_files(file1, file2, column, selected_columns)
     st.dataframe(merged)
 
 if st.sidebar.button("Download merged file"):
