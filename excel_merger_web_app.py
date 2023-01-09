4f90e6894665c0b60072b4e6a36c3731be5fd641
Georgios Tsirigos <geo.tsirigos@gmail.com>
2023-01-09 14:45:00 +0200

    Update excel_merger_web_app.py

--- excel_merger_web_app.py
+++ excel_merger_web_app.py
@@ -5,7 +5,7 @@ import base64
 
 from openpyxl.utils.dataframe import dataframe_to_rows
 
-def merge_files(file1, file2, column, selected_columns):
+def merge_files(file1, file2, column):
     # Check if file1 is a CSV file
     if file1.filename.endswith(".csv"):
         df1 = pd.read_csv(file1)
@@ -27,14 +27,7 @@ def merge_files(file1, file2, column, selected_columns):
         return
 
     merged = pd.merge(df1, df2, on=column, how='inner')
-    if selected_columns:
-        merged = merged[selected_columns]
     return merged
-
-def select_columns(dataframe):
-    all_columns = list(dataframe.columns)
-    selected_columns = st.multiselect("Select columns to include in merged file", all_columns)
-    return selected_columns
 
 def download_file(data, file_format):
     if file_format == "CSV":
@@ -59,15 +52,8 @@ file1 = st.sidebar.file_uploader("Upload first file")
 file2 = st.sidebar.file_uploader("Upload second file")
 column = st.sidebar.selectbox("Select column to merge on", [])
 
-select_columns = st.sidebar.checkbox("Select specific columns to include in merged file")
-
-if select_columns:
-    selected_columns = select_columns(merged)
-else:
-    selected_columns = None
-
 if st.sidebar.button("Merge files"):
-    merged = merge_files(file1, file2, column, selected_columns)
+    merged = merge_files(file1, file2, column)
     st.dataframe(merged)
 
 if st.sidebar.button("Download merged file"):
