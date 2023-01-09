d252114602ce77dd36eeda39eab1230971d5a148
Georgios Tsirigos <geo.tsirigos@gmail.com>
2023-01-09 10:11:07 +0200

    Update excel_merger_web_app.py

--- excel_merger_web_app.py
+++ excel_merger_web_app.py
@@ -1,19 +1,39 @@
 import streamlit as st
-import pandas as pd
-from openpyxl import load_workbook
 
-uploaded_file1 = st.file_uploader("Choose the first file to merge:", type=["xls", "xlsx", "csv"])
-uploaded_file2 = st.file_uploader("Choose the second file to merge:", type=["xls", "xlsx", "csv"])
-column_name = st.selectbox("Select the column to merge on:", df.columns)
-selected_columns = st.multiselect("Select the columns to export:", df.columns)
+st.title('Merge Excel Files')
 
-if st.button("Merge files"):
-    df1 = pd.read_excel(uploaded_file1)
-    df2 = pd.read_excel(uploaded_file2)
-    merged_df = pd.merge(df1, df2, on=column_name)
-    merged_df = merged_df[selected_columns]
-    if st.button("Download merged file"):
-        st.markdown("File is being downloaded...")
-        merged_df.to_excel("merged_file.xlsx", index=False)
-        st.markdown("File downloaded!")
-    st.dataframe(merged_df.head(20))
+st.write('Select the column to use as the key for merging:')
+key_column = st.selectbox('Column:', ['A', 'B', 'C'])
+
+st.write('Select the columns to export:')
+columns_to_export = st.multiselect('Columns:', ['A', 'B', 'C'])
+
+st.write('Select the input files:')
+files = st.file_uploader('Files:', type=['xls', 'xlsx', 'csv'])
+
+if files:
+    # Merge the files using pandas
+    df = pd.concat(map(pd.read_excel, files), ignore_index=True, sort=False)
+
+    # Select the columns to export
+    df = df[columns_to_export]
+
+    # Display the first 20 rows
+    st.dataframe(df.head(20))
+
+    # Add a download button
+    st.write('Click the button below to download the merged file:')
+    file_format = st.selectbox('Format:', ['xls', 'xlsx', 'csv'])
+    if st.button('Download'):
+        if file_format == 'xls':
+            with open('merged.xls', 'wb') as f:
+                f.write(df.to_excel())
+            st.success('File downloaded')
+        elif file_format == 'xlsx':
+            with open('merged.xlsx', 'wb') as f:
+                f.write(df.to_excel())
+            st.success('File downloaded')
+        elif file_format == 'csv':
+            with open('merged.csv', 'w') as f:
+                f.write(df.to_csv())
+            st.success('File downloaded')
