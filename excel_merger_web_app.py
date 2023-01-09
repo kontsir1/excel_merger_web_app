811dcc99b5c1803ed136085987a29ecfc5cdef1e
Georgios Tsirigos <geo.tsirigos@gmail.com>
2023-01-09 17:31:45 +0200

    Update excel_merger_web_app.py

--- excel_merger_web_app.py
+++ excel_merger_web_app.py
@@ -1,14 +1,16 @@
-import streamlit as st
-import pandas as pd
+import io
 
 file1 = st.file_uploader('Select the first file')
 file2 = st.file_uploader('Select the second file')
 
 if file1 is not None and file2 is not None:
-    df1 = pd.read_csv(file1)
-    df2 = pd.read_csv(file2)
+    file1.seek(0)
+    df1 = pd.read_csv(io.StringIO(file1.read()))
+    file2.seek(0)
+    df2 = pd.read_csv(io.StringIO(file2.read()))
     st.dataframe(df1)
     st.dataframe(df2)
+
     
 if st.button('Merge files'):
     merged_df = pd.merge(df1, df2)
