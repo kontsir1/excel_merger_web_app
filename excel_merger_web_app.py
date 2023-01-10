9015604d2e25a610c34b466b54ead79ee790cebc
Georgios Tsirigos <geo.tsirigos@gmail.com>
2023-01-10 20:30:35 +0200

    Update excel_merger_web_app.py

--- excel_merger_web_app.py
+++ excel_merger_web_app.py
@@ -72,21 +72,13 @@ def main():
         common_column1 = st.sidebar.selectbox("Select common column for first file", df1.columns)
         common_column2 = st.sidebar.selectbox("Select common column for second file", df2.columns)
         df_merged = merge_dataframes(df1, df2, common_column1, common_column2)
-        
-        # Change column name
-        column_to_change = st.sidebar.selectbox("Select column to change its name", df_merged.columns)
-        new_name = st.sidebar.text_input("Enter new name for the selected column")
-        if new_name:
-            if isinstance(new_name, str) and new_name.strip() and column_to_change:  # check if the input is not empty and selected columns
-                df_merged = df_merged.rename(columns={column_to_change:new_name})
 
+        # Display merged dataframe
+        st.markdown("## Merged dataframe")
+        st.write(df_merged)
 
-
-
-
-        
-        # Export selected columns of dataframe as CSV and XLSX
-        selected_columns = st.multiselect("Select columns to export", df_merged.columns, default=df_merged.columns)
+        # Select columns to export
+        selected_columns = st.sidebar.multiselect("Select columns to export", df_merged.columns)
 
         # Export dataframe as CSV
         if st.sidebar.button("Download CSV"):
