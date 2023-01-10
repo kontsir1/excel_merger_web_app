0d8179eda04aa161ee7e98e3172125182e7ec922
Georgios Tsirigos <geo.tsirigos@gmail.com>
2023-01-10 20:38:10 +0200

    Update excel_merger_web_app.py

--- excel_merger_web_app.py
+++ excel_merger_web_app.py
@@ -35,43 +35,6 @@ def merge_dataframes(df1, df2, common_column1, common_column2):
     df_merged.drop(common_column1, axis=1, inplace=True)
     return df_merged
 
-import streamlit as st
-import pandas as pd
-import base64
-import openpyxl
-from io import BytesIO
-
-def read_files():
-    """Read the first and second files as pandas dataframes"""
-    file_one = st.sidebar.file_uploader("Upload first file")
-    file_two = st.sidebar.file_uploader("Upload second file")
-    if file_one and file_two:
-        file_one_ext = file_one.name.split('.')[-1]
-        file_two_ext = file_two.name.split('.')[-1]
-
-        if file_one_ext in ['csv', 'xlsx']:
-            if file_one_ext == 'csv':
-                header_row_file1 = st.sidebar.selectbox("Select the header row of the First CSV file", list(range(1, 100)))
-                df1 = pd.read_csv(file_one, skiprows=header_row_file1-1, skipinitialspace=True)
-            else:
-                df1 = pd.read_excel(file_one)
-        
-        if file_two_ext in ['csv', 'xlsx']:
-            if file_two_ext == 'csv':
-                header_row_file2 = st.sidebar.selectbox("Select the header row of the Second CSV file", list(range(1, 100)))
-                df2 = pd.read_csv(file_two, skiprows=header_row_file2-1, skipinitialspace=True)
-            else:
-                df2 = pd.read_excel(file_two)
-        return df1, df2, file_one.name, file_two.name
-    else:
-        return None, None, None, None
-
-def merge_dataframes(df1, df2, common_column1, common_column2):
-    """Merge the two dataframes on the specified common columns"""
-    df_merged = pd.merge(df1, df2, left_on=common_column1, right_on=common_column2)
-    df_merged.drop(common_column1, axis=1, inplace=True)
-    return df_merged
-
 def export_dataframe(df, selected_columns):  
     """Export selected columns of dataframe as CSV and XLSX"""
     # Export as CSV
@@ -122,47 +85,6 @@ def main():
             export_dataframe(df_merged, selected_columns)
         if st.sidebar.button("Download Excel"):
             export_dataframe(df_merged, selected_columns)
-
-if __name__ == "__main__":
-    main()
-                     
-
-def main():
-    # Use the full page instead of a narrow central column
-    st.set_page_config(layout="wide")
-    
-    df1, df2, first_file_name, second_file_name = read_files()
-
-    if df1 is not None:
-        # Display the dataframes
-        col1, col2 = st.columns(2)
-        with col1:
-            st.markdown(f"## First file: {first_file_name}")
-            st.write(df1)
-        with col2:
-            st.markdown(f"## Second file: {second_file_name}")
-            st.write(df2)
-        
-        common_column1 = st.selectbox("Select the common column in the first file", df1.columns)
-        common_column2 = st.selectbox("Select the common column in the second file", df2.columns)
-        df_merged = merge_dataframes(df1, df2, common_column1, common_column2)
-        
-        selected_columns = st.multiselect("Select the columns to export", df_merged.columns)
-        if st.button("Export"):
-            file_type = st.selectbox("Select the file type for export", ["CSV", "XLSX"])
-            if file_type == "CSV":
-                csv = df_merged[selected_columns].to_csv(index=False)
-                b64 = base64.b64encode(csv.encode()).decode()
-                csv_href = f'<a href="data:file/csv;base64,{b64}" download="merged_data.csv">Download CSV</a>'
-                st.sidebar.markdown(csv_href, unsafe_allow_html=True)
-            elif file_type == "XLSX":
-                xlsx_file = BytesIO()
-                df_merged[selected_columns].to_excel(xlsx_file, index=False)
-                xlsx_file.seek(0)
-                xlsx_b64 = base64.b64encode(xlsx_file.read()).decode()
-                xlsx_href = f'<a href="data:file/xlsx;base64,{xlsx_b64}" download="merged_data.xlsx">Download XLSX</a>'
-                st.sidebar.markdown(xlsx_href, unsafe_allow_html=True)
-
 
 if __name__ == "__main__":
     main()
