872f5ceb08f7793ff1f5e40184876ab8b62d919c
Georgios Tsirigos <geo.tsirigos@gmail.com>
2023-01-10 12:19:40 +0200

    Update excel_merger_web_app.py

--- excel_merger_web_app.py
+++ excel_merger_web_app.py
@@ -9,8 +9,20 @@ def read_files():
     file_one = st.sidebar.file_uploader("Upload first file")
     file_two = st.sidebar.file_uploader("Upload second file")
     if file_one and file_two:
-        df1 = pd.read_csv(file_one)
-        df2 = pd.read_csv(file_two)
+        file_one_ext = file_one.name.split('.')[-1]
+        file_two_ext = file_two.name.split('.')[-1]
+
+        if file_one_ext in ['csv', 'xlsx']:
+            if file_one_ext == 'csv':
+                df1 = pd.read_csv(file_one)
+            else:
+                df1 = pd.read_excel(file_one)
+        
+        if file_two_ext in ['csv', 'xlsx']:
+            if file_two_ext == 'csv':
+                df2 = pd.read_csv(file_two)
+            else:
+                df2 = pd.read_excel(file_two)
         return df1, df2, file_one.name, file_two.name
     else:
         return None, None, None, None
