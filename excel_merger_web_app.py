31bdbeafef1e26f83626091e47fd90a6ffb66b33
Georgios Tsirigos <geo.tsirigos@gmail.com>
2023-01-11 10:38:06 +0200

    Update excel_merger_web_app.py

--- excel_merger_web_app.py
+++ excel_merger_web_app.py
@@ -3,7 +3,6 @@ import pandas as pd
 import base64
 import openpyxl
 from io import BytesIO
-import csvkit as csv
 
 def read_files():
     """Read the first and second files as pandas dataframes"""
@@ -16,21 +15,19 @@ def read_files():
         if file_one_ext in ['csv', 'xlsx']:
             if file_one_ext == 'csv':
                 header_row_file1 = st.sidebar.selectbox("Select the header row of the First CSV file", list(range(1, 100)))
-                df1 = pd.read_csv(file_one, skiprows=header_row_file1-1,  skipinitialspace=True)
-                bad_lines = pd.DataFrame(csv.reader(file_one))
+                df1 = pd.read_csv(file_one, skiprows=header_row_file1-1, skipinitialspace=True)
             else:
                 df1 = pd.read_excel(file_one)
         
         if file_two_ext in ['csv', 'xlsx']:
             if file_two_ext == 'csv':
                 header_row_file2 = st.sidebar.selectbox("Select the header row of the Second CSV file", list(range(1, 100)))
-                df2 = pd.read_csv(file_two, skiprows=header_row_file2-1,  skipinitialspace=True)
-                bad_lines = pd.DataFrame(csv.reader(file_two))
+                df2 = pd.read_csv(file_two, skiprows=header_row_file2-1, skipinitialspace=True)
             else:
                 df2 = pd.read_excel(file_two)
-        return df1, df2, file_one.name, file_two.name, bad_lines
+        return df1, df2, file_one.name, file_two.name
     else:
-        return None, None, None, None, None
+        return None, None, None, None
 
 def merge_dataframes(df1, df2, common_column1, common_column2):
     """Merge the two dataframes on the specified common columns"""
