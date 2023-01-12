5701bcd213675363ce00e271efae1c26b97d33bc
Georgios Tsirigos <geo.tsirigos@gmail.com>
2023-01-12 17:30:30 +0200

    Update excel_merger_web_app.py

--- excel_merger_web_app.py
+++ excel_merger_web_app.py
@@ -37,17 +37,22 @@ def read_excel(file):
 @st.cache
 def read_files(file_one,file_two):
     """Read the first and second files as pandas dataframes"""
-    file_one_ext = file_one.name.split('.')[-1]
-    file_two_ext = file_two.name.split('.')[-1]
-    if file_one_ext == 'csv':
-        df1 = read_csv(file_one)
+    if file_one and file_two:
+        file_one_ext = file_one.name.split('.')[-1]
+        file_two_ext = file_two.name.split('.')[-1]
+        df1 = pd.DataFrame()
+        df2 = pd.DataFrame()
+        if file_one_ext == 'csv':
+            df1 = read_csv(file_one)
+        else:
+            df1 = read_excel(file_one)
+        if file_two_ext == 'csv':
+            df2 = read_csv(file_two)
+        else:
+            df2 = read_excel(file_two)
+        return df1, df2, file_one.name, file_two.name
     else:
-        df1 = read_excel(file_one)
-    if file_two_ext == 'csv':
-        df2 = read_csv(file_two)
-    else:
-        df2 = read_excel(file_two)
-    return df1, df2, file_one.name, file_two.name
+        return None, None, None, None
 
 def merge_dataframes(df1, df2, common_column1, common_column2):
     df_merged = None
