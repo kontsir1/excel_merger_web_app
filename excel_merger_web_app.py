4a4f895614974099b8c303e1f3d82a54c52c04b3
Georgios Tsirigos <geo.tsirigos@gmail.com>
2023-01-17 12:30:32 +0200

    Update excel_merger_web_app.py

--- excel_merger_web_app.py
+++ excel_merger_web_app.py
@@ -69,7 +69,7 @@ def export_dataframe(df, selected_columns):
         csv = df[selected_columns].to_csv(index=False)
         b64 = base64.b64encode(csv.encode()).decode()
         csv_href = f'<a href="data:file/csv;base64,{b64}" download="merged_data.csv">Download CSV</a>'
-        #st.sidebar.markdown(csv_href, unsafe_allow_html=True)
+        st.sidebar.markdown(csv_href, unsafe_allow_html=True)
 
         # Export as XLSX
         xlsx_file = BytesIO()
@@ -77,7 +77,7 @@ def export_dataframe(df, selected_columns):
         xlsx_file.seek(0)
         xlsx_b64 = base64.b64encode(xlsx_file.read()).decode()
         xlsx_href = f'<a href="data:file/xlsx;base64,{xlsx_b64}" download="merged_data.xlsx">Download XLSX</a>'
-        #st.sidebar.markdown(xlsx_href, unsafe_allow_html=True)
+        st.sidebar.markdown(xlsx_href, unsafe_allow_html=True)
                      
 
 def main():
